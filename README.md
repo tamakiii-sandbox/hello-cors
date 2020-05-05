5046a9cba79ea0346a870e8c266ffe5035a7d627
tamakiii <tamakiii@users.noreply.github.com>
2020-05-06 00:38:26 +0900

    production/development targeting & run on host-os mode

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # hello-cors
+
+## How to use
+```sh
+make -f docker.mk install-dev
+make -f docker.mk build && docker-compose up
+```
+
+## Running webpack-dev-server on host-OS
+`docker-compose.override.yml`
+```yml
+version: "3.7"
+services:
+  node:
+    entrypoint: "false"
+```
+```sh
+make -f docker.mk clean install-dev NODE_HOST=host.docker.internal
+make -C front build-dev
+make -f docker.mk build && docker-compose up
+```
+```sh
+docker-compose run --rm nginx cat /etc/nginx/conf.d/default.conf | grep proxy_pass
+```
--- docker.mk
+++ docker.mk
@@ -1,7 +1,9 @@
 .PHONY: help install dependencies clean
 
+ENVIRONMENT := production-pseudo
 NGINX_PORT := 8080
 NODE_PORT := 8888
+NODE_HOST := node
 
 export DOCKER_BUILDKIT := 1
 export COMPOSE_DOCKER_CLI_BUILD := 1
@@ -13,16 +15,26 @@ install: \
 	.env \
 	dependencies
 
+install-dev: \
+	development \
+	install
+
 dependencies:
 	type docker > /dev/null
 	type docker-compose > /dev/null
 
 .env:
-	echo "NGINX_PORT=$(NGINX_PORT)" > $@
+	echo "ENVIRONMENT=$(ENVIRONMENT)" > $@
+	echo "NGINX_PORT=$(NGINX_PORT)" >> $@
 	echo "NODE_PORT=$(NODE_PORT)" >> $@
+	echo "NODE_HOST=$(NODE_HOST)" >> $@
 
 build:
 	docker-compose build
 
 clean:
 	rm -rf .env
+
+.PHONY: development
+development:
+	$(eval ENVIRONMENT := $@)
